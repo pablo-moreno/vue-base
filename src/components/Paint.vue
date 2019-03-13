faf396d5481e49e3b8d1c724c6dac14092e89d4f
Pablo Moreno <pablo@kasfactory.net>
2019-03-13 16:05:49 +0100

    Paint working

--- src/components/Paint.vue
+++ src/components/Paint.vue
@@ -1,9 +1,18 @@
 <template>
-<div>
+<div class="paint">
+  
   <header>
-    <button @click="clear">Clear</button>
-    <input type="color" name="color-selector" v-model="color">
+    <div class="paint-header" :style="{'width': `${width}px`}" >
+      <div class="color-list">
+        <div class="paint-color" v-for="(mColor, i) in colors" :key="`color-${i}`" @click="color = mColor" :style="{'background': mColor}" />
+      </div>
+      <div>
+        <input type="color" name="color-selector" v-model="color" @change="saveColor">
+        <button @click="saveColor">Save</button>
+      </div>
+    </div>
   </header>
+
   <canvas 
     ref="the-canvas" 
     @mousedown="mouseDown" 
@@ -12,8 +21,8 @@
     @mouseleave="mouseLeave"
     :style="{'width': `${width}px`, 'height': `${height}px`}"
   >
-
   </canvas>
+  <!-- <button @click="clear">Clear</button> -->
 </div>
 </template>
 
@@ -33,11 +42,14 @@ export default {
     return {
       context: undefined,
       paint: false,
-      clickX: [],
-      clickY: [],
-      clickDrag: [],
-      points: {},
+      clicks: [],
       color: '#df4b26',
+      colors: [
+        '#df4b26',
+        '#235b26',
+        '#982828',
+        '#cfcfcf',
+      ],
       lineStyle: 'round',
       lineWidth: 5,
       rect: undefined,
@@ -65,14 +77,14 @@ export default {
       const [x, y] = this.getMousePosition(event)
 
       this.paint = true
-      this.addClick(x, y)
+      this.addClick(x, y, this.color, false)
       this.redraw()
     },
     mouseMove(event) {
       if (this.paint) {
         const [x, y] = this.getMousePosition(event)
 
-        this.addClick(x, y, true)
+        this.addClick(x, y, this.color, true)
         this.redraw()
       }
     },
@@ -82,51 +94,73 @@ export default {
     mouseLeave(event) {
       this.paint = false
     },
-    addClick(x, y, dragging) {
-      this.clickX.push(x)
-      this.clickY.push(y)
-      this.clickDrag.push(dragging)
+    addClick(x, y, color, drag) {
+      this.clicks.push({ x, y, color, drag })
     },
     redraw() {
       const canvas = this.$refs['the-canvas']
       const rect = canvas.getBoundingClientRect()
       this.context.clearRect(0, 0, rect.width, rect.height)
-      this.context.strokeStyle = this.color
+      
       this.context.lineJoin = this.lineStyle
       this.context.lineWidth = this.lineWidth
 
-      for (let i = 0; i < this.clickX.length; i++) {
-        this.context.fillStyle = 'white'
+      this.clicks.forEach((click, i) => {
+        this.context.strokeStyle = click.color
         this.context.beginPath()
 
-        if (this.clickDrag[i] && i) {
-          this.context.moveTo(this.clickX[i - 1], this.clickY[i - 1])
+        if (click.drag && i > 0) {
+          this.context.moveTo(this.clicks[i - 1].x, this.clicks[i - 1].y)
         }
         else {
-          this.context.moveTo(this.clickX[i] - 1, this.clickY[i])
+          this.context.moveTo(this.clicks[i].x - 1, this.clicks[i].y)
         }
 
-        this.context.lineTo(this.clickX[i], this.clickY[i])
+        this.context.lineTo(this.clicks[i].x, this.clicks[i].y)
         this.context.closePath()
         this.context.stroke()
-        
-      }
+      })
     },
     clear() {
       const { width, height } = this.context.canvas
       this.context.clearRect(0, 0, width, height)
-      this.clickX = []
-      this.clickY = []
-      this.clickDrag = []
+      this.clicks = []
+    },
+    saveColor() {
+      if (this.colors.indexOf(this.color) === -1)
+        this.colors.push(this.color)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: center;
+}
 
 canvas {
   border: solid 1px rgba(255,255,255, 0.1);
   background: white;
+}
+
+.paint-color {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  cursor: pointer;
+  margin: 0 0.5em 0.5em 0;
+}
+
+.paint-header {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+}
+
+.color-list {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
